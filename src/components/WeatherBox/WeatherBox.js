b9aa9828e8b967b5a20bebfdd714a2fbff9a4257
Patryk Szwiec <patryk.szwiec@gmail.com>
2023-06-16 13:19:34 +0200

    Add error component render to weatherBox

--- src/components/WeatherBox/WeatherBox.js
+++ src/components/WeatherBox/WeatherBox.js
@@ -1,14 +1,18 @@
 import PickCity from '../PickCity/PickCity';
 import WeatherSummary from '../WeatherSummary/WeatherSummary';
 import Loader from '../Loader/Loader';
+import ErrorBox from '../ErrorBox/ErrorBox';
 
 import { useCallback, useState} from 'react';
 
-const WeatherBox = props => {
+const WeatherBox = () => {
 
   const [weather, setWeather] = useState();
+
   //state for verify if we wait for infomation from server api
   const [pending, setPending] = useState(false);
+
+  const [error, setError] = useState(false);
 
   // eslint-disable-next-line
   const handleCityChange = useCallback((city) => {
@@ -16,8 +20,12 @@ const WeatherBox = props => {
     setPending(true);
 
     fetch(`http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=32fe6d9b5e49112a35dd803d0c5e160c&units=metric`)
-    .then(res => res.json())
-      .then(data => {
+    .then(res => {
+      if(res.status === 200) {
+        setError(false);
+        return res.json()
+
+        .then(data => {
           const weatherData = {
             city: data.name,
             temp: data.main.temp,
@@ -26,14 +34,19 @@ const WeatherBox = props => {
           };
           setWeather(weatherData);
           setPending(false);
-      });
+        });
+      } else {
+      setError(true);
+      }
+    });
   });
 
   return (
     <section>
       <PickCity action={handleCityChange} />
       {(weather && !pending) && <WeatherSummary {...weather} />}
-      {(pending && pending) && <Loader />}
+      {error && <ErrorBox>There is no such city!</ErrorBox>}
+      {(pending && pending && !error) && <Loader />}
     </section>
   )
 };
