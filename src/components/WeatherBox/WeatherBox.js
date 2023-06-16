ef2987e82dc231e072f7bdb6027619dfc264e792
Patryk Szwiec <patryk.szwiec@gmail.com>
2023-06-16 13:10:19 +0200

    Add render for weatherSummary and Loader with if statement

--- src/components/WeatherBox/WeatherBox.js
+++ src/components/WeatherBox/WeatherBox.js
@@ -7,9 +7,14 @@ import { useCallback, useState} from 'react';
 const WeatherBox = props => {
 
   const [weather, setWeather] = useState();
+  //state for verify if we wait for infomation from server api
+  const [pending, setPending] = useState(false);
 
   // eslint-disable-next-line
   const handleCityChange = useCallback((city) => {
+
+    setPending(true);
+
     fetch(`http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=32fe6d9b5e49112a35dd803d0c5e160c&units=metric`)
     .then(res => res.json())
       .then(data => {
@@ -19,16 +24,16 @@ const WeatherBox = props => {
             icon: data.weather[0].icon,
             description: data.weather[0].main
           };
-          console.log(weatherData);
           setWeather(weatherData);
+          setPending(false);
       });
   });
 
   return (
     <section>
       <PickCity action={handleCityChange} />
-      <WeatherSummary {...weather} />
-      <Loader />
+      {(weather && !pending) && <WeatherSummary {...weather} />}
+      {(pending && pending) && <Loader />}
     </section>
   )
 };
